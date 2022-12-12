ee02c476677b8c9ec8ddc440a15e051d4038e9c2
Jithin C <jith97972@gmail.com>
2022-12-12 19:36:34 +0530

    atlas

--- confi/connection.js
+++ confi/connection.js
@@ -3,7 +3,7 @@ const state={
     db:null
 }
 module.exports.connect=function(done){
-    const url='mongodb://localhost:27017'
+    const url='mongodb+srv://jithin97972:jithin1234@cluster0.ldzzlzo.mongodb.net/test'
     const dbname='project'
 
     MongoClient.connect(url,(err,data)=>{
